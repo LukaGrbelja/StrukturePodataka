0b63db59e6cc6e5cf3b73b9bce065a0025826dc1
LukaGrbelja <hrvoje.bing@gmail.com>
2024-10-07 19:49:25 +0200

    19:49

--- Vj1/Source.cpp
+++ Vj1/Source.cpp
@@ -1,6 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+
+#define MAXL 1024;
+
+int countNumOfLines(const char*);
 
 int main() {
-	printf("Hello world\n");
+
+	int numOfLines = countNumOfLines(".txt");
+
+	return 0;
+}
+
+int countNumOfLines(const char* fileName) {
+	FILE* fp = NULL;
+	int numOfLines = 0;
+	char buffer[MAXL] = "asdad";
+	
+	fp = fopen("Studenti.txt", "r");
+
+	if (!fp) {
+		return -1;
+	}
+
+	while (!feof(fp)) {
+		fgets(buffer);
+		numOfLines++;
+	}
+
 	return 0;
 }
