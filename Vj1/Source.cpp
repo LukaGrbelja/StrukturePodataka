253f8654af35b5a17fec6746bcfd9cfeca2b5932
LukaGrbelja <luka.grbelja@gmail.com>
2024-10-14 18:21:50 +0200

    DZ

--- Vj1/Source.cpp
+++ Vj1/Source.cpp
@@ -2,32 +2,70 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MAXL 1024;
+#define MAX 1024;
 
-int countNumOfLines(const char*);
+int NumberOfRows(const char*);
+void DataDisplay(int, const char*);
 
-int main() {
+typedef struct{
+    char ime[20] = {};
+    char prezime[20] = {};
+    int bodovi = 0;
+    int rel_bodovi = 0;
+}Student;
 
-	int numOfLines = countNumOfLines(".txt");
+int main(){
 
-	return 0;
+    int number = 0;
+
+    number = NumberOfRows("datoteka.txt");
+    printf("There are %d lines in the file.\n", number);
+
+    DataDisplay(number, "datoteka.txt");
+
+    return 0;
 }
 
-int countNumOfLines(const char* fileName) {
-	FILE* fp = NULL;
-	int numOfLines = 0;
-	char buffer[MAXL] = "asdad";
-	
-	fp = fopen("Studenti.txt", "r");
+int NumberOfRows(const char* filename){
+    FILE* fp = NULL;
+    fp = fopen(filename, "r");
+    char buffer[MAX] = {};
+    int count = 0;
 
-	if (!fp) {
-		return -1;
-	}
+    if(!fp){
+        printf("File opening error\n");
+        return -1;
+    }
 
-	while (!feof(fp)) {
-		fgets(buffer);
-		numOfLines++;
-	}
+    while(!feof(fp)){
+        fgets(buffer, MAX, fp);
+        count++;
+    }
+    fclose(fp);
+    return count;
+}
 
-	return 0;
+void DataDisplay (int n, const char* filename){
+    FILE* fp = NULL;
+    fp = fopen(filename, "r");
+    Student *list = {};
+    int max_bodovi = 20;
+
+    if(!fp){
+        printf("File opening error\n");
+        return;
+    }
+
+    list = (Student*)malloc(n*sizeof(Student));
+
+    for(int i=0;i<n;i++){
+        fscanf(fp, "%s %s %d", list[i].ime, list[i].prezime, &list[i].bodovi);
+    }
+
+    printf("IME\tPREZIME\tBODOVI\tRELATIVNI BODOVI\n");
+    for(int j=0;j<n;j++){
+        printf("%s\t%s\t%d\t%f\n", list[j].ime, list[j].prezime, list[j].bodovi, (float)list[j].bodovi/max_bodovi*100);
+    }
+
+    fclose(fp);
 }
