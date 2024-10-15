53bfdc841453aedd606864799b2fd2feba565c3b
LukaGrbelja <luka.grbelja@gmail.com>
2024-10-15 20:05:23 +0200

    Vj2 - ispis

--- Vj2/Source.cpp
+++ Vj2/Source.cpp
@@ -1,17 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-typedef struct _osoba {
-    char ime[20];
-    char prezime[20];
-    int godinaRodenja;
-} Osoba;
+#include<cstring>
 
 struct clanListe;
 typedef struct clanListe* adresaClana;
 typedef struct clanListe {
+    char ime[20];
+    char prezime[20];
+    int godinaRodenja;
     adresaClana prev;
-    Osoba strosoba;
     adresaClana next;
 } ClanListe;
 
@@ -20,28 +17,60 @@ typedef struct lista {
     adresaClana zadnji;
 } Lista;
 
-Osoba stvoriOsobu();
-adresaClana stvoriClana(adresaClana, Osoba, adresaClana);
-void dodajClanaNaPocetak(Lista);
+adresaClana stvoriClanaListe();
+void dodajClanaNaPocetak(Lista*);
+void ispisClanova(Lista*);
 
 int main() {
     Lista osobe = { NULL, NULL };
-    dodajClanaNaPocetak(osobe);
+    ispisClanova(&osobe);
+    dodajClanaNaPocetak(&osobe);
+    ispisClanova(&osobe);
+    dodajClanaNaPocetak(&osobe);
+    ispisClanova(&osobe);
     return 0;
 }
 
-Osoba stvoriOsobu() {
-    Osoba placeholder = { "Ime", "Prezime", 2025 };
-    return placeholder;
+adresaClana stvoriClanaListe() {
+    
+    adresaClana clan = NULL;
+	char ime[20], prezime[20];
+	int godrod;
+	
+	clan = (adresaClana)malloc(sizeof(ClanListe));
+	if (clan == NULL) {
+		printf("Error allocating memory");
+		return NULL;
+	}
+	
+	scanf("%s", ime);
+	strcpy(clan->ime, ime);
+	scanf("%s", prezime);
+	strcpy(clan->prezime, prezime);
+	scanf("%d", &godrod);
+	clan->godinaRodenja = godrod;
+	return clan;
 }
 
-adresaClana stvoriClana(adresaClana prev, Osoba osoba, adresaClana zadnji) {
-    ClanListe clan = { prev, osoba, zadnji };
-    adresaClana aClan = &clan;
-    return aClan;
+void dodajClanaNaPocetak(Lista* osobe) {
+    adresaClana osoba = stvoriClanaListe();
+    if(osobe->prvi == NULL) {
+        osobe->prvi = osoba;
+        osobe->zadnji = osoba;
+    }
+    else{
+        adresaClana temp = osobe->prvi;
+        temp->prev = osoba;
+        osoba->next = temp;
+        osobe->prvi = osoba;
+    }
 }
 
-void dodajClanaNaPocetak(Lista osobe) {
-    Osoba prvaOsoba = stvoriOsobu();
-    osobe.prvi = stvoriClana(osobe.prvi, prvaOsoba, osobe.zadnji);
+void ispisClanova(Lista* osobe) {
+    printf("Ispis:\n");
+    adresaClana temp = osobe->prvi;
+    while (temp != NULL) {
+        printf("%s %s %d\n", temp->ime, temp->prezime, temp->godinaRodenja);
+        temp = temp->next;
+    }
 }
